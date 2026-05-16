f26a900cb06c5b715a517fe7fa9cf162b5e9094f
Jamile <jamile.bastos.dev@gmail.com>
2026-05-16 16:21:07 -0300

    chore

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const prefixHelloPortuguese = "Hello, "
+
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "World"
+	}
+	return prefixHelloPortuguese + name
 }
 
 func main() {
--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	result := Hello("Jamile")
-	expected := "Hello, Jamile"
 
-	if result != expected {
-		t.Errorf("result '%s', expected '%s'", result, expected)
+	verifyCorrectMessage := func(t *testing.T, result, expected string) {
+		t.Helper()
+		if result != expected {
+			t.Errorf("result '%s', expected '%s'", result, expected)
+		}
 	}
+
+	t.Run("say hello to people", func(t *testing.T) {
+		result := Hello("Jamile")
+		expected := "Hello, Jamile"
+
+		verifyCorrectMessage(t, result, expected)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is sent", func(t *testing.T) {
+		result := Hello("")
+		expected := "Hello, World"
+
+		verifyCorrectMessage(t, result, expected)
+	})
 }
